ebd0d09d8bd7aaa4cec34f4f7e1a2c8c150be37d
Edward Anderson <nilbus@nilbus.com>
2014-10-11 15:36:30 -0400

    Set up gorm ORM and create the basic data structures

---
+++ db.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func ConnectDB() {
+	dbFile := "db.sqlite3"
+	db, err := gorm.Open("sqlite3", dbFile)
+	if err != nil {
+		log.Fatal("Failed to open the database " + dbFile)
+	}
+	db.SetLogger(log.New(os.Stdout, "\n", 0))
+	db.LogMode(true)
+	db.AutoMigrate(&Contestant{}, &Heat{}, &Run{})
+}
+
+type Contestant struct {
+	Id      int32
+	Name    string
+	Retured bool
+	Runs    []Run
+}
+
+type Heat struct {
+	Id           int32
+	Sequence     int32
+	Status       string
+	ContestantId int32
+	Runs         []Run
+}
+
+type Run struct {
+	Id           int32
+	Time         float32
+	Lane         int32
+	ContestantId int32
+	HeatId       int32
+}
