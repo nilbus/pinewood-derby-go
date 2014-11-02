bbf010be4b2f656c2ce8e0cd4a1f97f63e2ea0cc
Edward Anderson <nilbus@nilbus.com>
2014-11-02 06:38:24 -0500

    Add a function for testing code
    I wish there was a go repl.

--- pinewood-derby.go
+++ pinewood-derby.go
@@ -3,5 +3,17 @@ package main
 func main() {
 	loadDerbyConfig()
 	ConnectDB()
+	test()
 	ServeWeb()
+}
+
+func test() {
+	// log.Fatal("derbyConfig: ", iter.N(derbyConfig["laneCount"].(int)))
+	// var i int
+	// var r []struct{}
+	// r = iter.N(9)
+	// for i = range r {
+	// 	fmt.Printf("n: %d\n", i+1)
+	// }
+	// log.Fatal("done")
 }
