13331bd26c7131aba4bd41a907f6940baec809f5
Edward Anderson <nilbus@nilbus.com>
2014-11-02 20:54:27 -0500

    Split web.go into web-actions and web-helpers

---
+++ web-actions.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func configureRoutes() {
+	http.HandleFunc("/", handleWelcome)
+	restfulResource("contestants")
+}
+
+func handleWelcome(w http.ResponseWriter, r *http.Request) {
+	render("welcome", w, nil)
+}
+
+var restfulHandlers = map[string]restfulHandlerFunc{
+	"handleContestantsIndex":  handleContestantsIndex,
+	"handleContestantsNew":    handleContestantsNew,
+	"handleContestantsCreate": handleContestantsCreate,
+	"handleContestantsShow":   handleContestantsShow,
+	"handleContestantsEdit":   handleContestantsEdit,
+	"handleContestantsUpdate": handleContestantsUpdate,
+	"handleContestantsDelete": handleContestantsDelete,
+}
+
+func handleContestantsIndex(w http.ResponseWriter, r *http.Request, id string) {
+	var contestants []Contestant
+	db.Find(&contestants)
+	render("contestants/index", w, contestants)
+}
+
+func handleContestantsNew(w http.ResponseWriter, r *http.Request, id string) {
+	render("contestants/new", w, nil)
+}
+
+func handleContestantsCreate(w http.ResponseWriter, r *http.Request, id string) {
+	contestantName := r.PostFormValue("name")
+	contestant := Contestant{Name: contestantName}
+	db.Create(&contestant)
+	render("contestants/new", w, "Registered "+contestantName)
+}
+
+func handleContestantsShow(w http.ResponseWriter, r *http.Request, id string) {
+	fmt.Fprint(w, "contestants show "+id)
+}
+
+func handleContestantsEdit(w http.ResponseWriter, r *http.Request, id string) {
+	fmt.Fprint(w, "contestants edit "+id)
+}
+
+func handleContestantsUpdate(w http.ResponseWriter, r *http.Request, id string) {
+	fmt.Fprint(w, "contestants update "+id)
+}
+
+func handleContestantsDelete(w http.ResponseWriter, r *http.Request, id string) {
+	fmt.Fprint(w, "contestants delete "+id)
+}
